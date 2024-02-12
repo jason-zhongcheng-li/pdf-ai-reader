38a5f8f0278c0e36dc92f301842fdffccccfded1
ZHONGCHENG LI <zhongcheng@192-168-1-103.tpgi.com.au>
2024-02-13 00:54:26 +1100

    feat: install shadcn-ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
-import MaxWithWrapper from "./components/MaxWidthWrapper";
+import Link from "next/link";
+import MaxWithWrapper from "../components/MaxWidthWrapper";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -8,6 +11,21 @@ export default function Home() {
           PDF reader is public
         </p>
       </div>
+      <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+        Chat with your <span className="text-blue-600">Documents</span> in a
+        second
+      </h1>
+      <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
+        PDF AI allows you to have conversatoin with any PDF document. Simply
+        upload your file and start asking question right away.
+      </p>
+      <Link
+        className={buttonVariants({ size: "lg", className: "mt-5" })}
+        href="/dashboard"
+        target="_blank"
+      >
+        Get started <ArrowRightIcon className="ml-2 h-5 w-5 font-bold" />
+      </Link>
     </MaxWithWrapper>
   );
 }
